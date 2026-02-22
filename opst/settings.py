1dc7590292286170aaf8a9c72a2cb56fb8172b17
Astha Dwivedi <asthaparauha@gmail.com>
2026-02-22 11:51:55 +0530

    Update settings.py- database variable

--- opst/settings.py
+++ opst/settings.py
@@ -102,7 +102,9 @@ WSGI_APPLICATION = "opst.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
